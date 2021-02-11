7ed8685caa528ed3f97a505d47137bb9bca4f2f2
yukari638 <khmk0118@gmail.com>
2021-02-12 01:47:48 +0900

    Display of add.html

--- src/main/java/com/example/demo/controller/HeroController.java
+++ src/main/java/com/example/demo/controller/HeroController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.dto.HeroRequest;
 import com.example.demo.entity.Hero;
 import com.example.demo.mapper.HeroMapper;
 
@@ -20,11 +23,25 @@ public class HeroController {
 	@Autowired
 	HeroMapper heroMapper;
 	
+	/*
+	 * ヒーロー情報一覧表示
+	 */
 	@GetMapping(value = "/")
 	public String heroList(Model model) {
 		List<Hero> list = heroMapper.selectAll();
 		model.addAttribute("list", list);
 		return "index";
+	}
+	
+	/**
+	 * ユーザー新規登録画面を表示
+	 * @param model Model
+	 * @return ユーザー情報一覧画面
+	 */
+	@RequestMapping(value = "hero/add", method = RequestMethod.GET)
+	public String displayAdd(Model model) {
+		model.addAttribute("heroRequest", new HeroRequest());
+		return "hero/add";
 	}
 	
 }

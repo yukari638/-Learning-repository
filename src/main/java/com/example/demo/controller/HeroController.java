3f76f073ffb7a36c67bde3f26be92679c3d054a4
yukari638 <khmk0118@gmail.com>
2021-02-16 10:23:03 +0900

    Revert "Change & Clean:"
    This reverts commit 2cb5b16cb3855d22e05c612176fa658e4794c40c.

--- src/main/java/com/example/demo/controller/HeroController.java
+++ src/main/java/com/example/demo/controller/HeroController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.example.demo.dto.HeroForm;
+import com.example.demo.dto.HeroRequest;
 import com.example.demo.entity.Hero;
 import com.example.demo.mapper.HeroMapper;
 
@@ -41,20 +41,20 @@ public class HeroController {
 	 */
 	@RequestMapping(value = "/hero/add", method = RequestMethod.GET)
 	public String heroAdd(Model model) {
-		model.addAttribute("heroForm", new HeroForm());
+		model.addAttribute("heroRequest", new HeroRequest());
 		return "hero/add";
 	}
 	
 	/**
 	 * ヒーロー新規登録
-	 * @param heroForm リクエストデータ
+	 * @param heroRequest リクエストデータ
 	 * @param model Model
 	 * @return ヒーロー情報一覧画面
 	 */
 	@RequestMapping(value="/hero/create", method=RequestMethod.POST)
-    public String heroCreate(@ModelAttribute HeroForm heroForm, Model model) {
+    public String heroCreate(@ModelAttribute HeroRequest heroRequest, Model model) {
 		// ヒーロー情報の登録
-		heroMapper.insert(heroForm);
+		heroMapper.insert(heroRequest);
 		return "redirect:/index";
 	}
 	

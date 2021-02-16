5faaba11bb4efd783d4aa1ee87db20e3de392891
yukari638 <khmk0118@gmail.com>
2021-02-16 11:02:22 +0900

    change:
    getter & setter create
    (lombok repeal)

--- src/main/java/com/example/demo/dto/HeroForm.java
+++ src/main/java/com/example/demo/dto/HeroForm.java
@@ -7,7 +7,6 @@ import lombok.Data;
  * ヒーロー情報　リクエストデータ
  */
 
-@Data
 public class HeroForm {
 
 	/*
@@ -15,29 +14,76 @@ public class HeroForm {
 	 */
 	private Integer id;
 	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
 	/*
 	 * ヒーロー名
 	 */
 	private String name;
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	/*
 	 * ヒーローパワー名
 	 */
 	private String powerName;
 	
+	public String getPowerName() {
+		return powerName;
+	}
+
+	public void setPowerName(String powerName) {
+		this.powerName = powerName;
+	}
+
 	/*
 	 * ヒーローパワー発動型、コスト
 	 */
 	private String powerActivate;
 	
+	public String getPowerActivate() {
+		return powerActivate;
+	}
+
+	public void setPowerActivate(String powerActivate) {
+		this.powerActivate = powerActivate;
+	}
+
 	/*
-	 * ヒーローパワー
+	 * ヒーローパワー内容
 	 */
 	private String power;
 	
+	public String getPower() {
+		return power;
+	}
+
+	public void setPower(String power) {
+		this.power = power;
+	}
+
 	/*
 	 * ヒーローBan種族
 	 */
 	private String typeBan;
 	
+	public String getTypeBan() {
+		return typeBan;
+	}
+
+	public void setTypeBan(String typeBan) {
+		this.typeBan = typeBan;
+	}	
 }

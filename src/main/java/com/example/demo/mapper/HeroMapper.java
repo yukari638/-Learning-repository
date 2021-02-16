4d43570cec17b4d1bbff0e2e78dbf6af59d9d2b5
yukari638 <khmk0118@gmail.com>
2021-02-16 09:52:58 +0900

    Change & Clean :
    HeroRequest → HeroForm (import class name, in method)
    Clean : heroRequest → heroFrom

--- src/main/java/com/example/demo/mapper/HeroMapper.java
+++ src/main/java/com/example/demo/mapper/HeroMapper.java
@@ -3,7 +3,7 @@ package com.example.demo.mapper;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
-import com.example.demo.dto.HeroRequest;
+import com.example.demo.dto.HeroForm;
 import com.example.demo.entity.Hero;
 
 /*
@@ -18,6 +18,6 @@ public interface HeroMapper {
 	List<Hero> selectAll();
 	
 	//Table:herosに新規登録
-	void insert(HeroRequest heroRequest);
+	void insert(HeroForm heroFrom);
 
 }

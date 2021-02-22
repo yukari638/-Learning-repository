9f58276c412304865d1e39fd2480d591a03a1b88
yukari638 <khmk0118@gmail.com>
2021-02-22 12:16:05 +0900

    追加：
    ・@Autowired　HeroListService service　追加
    ・「HeroControllerクラス」に書いていた新規登録画面のメソッドをこのクラスに移動。
    　元の内容から変更はなし。

--- src/main/java/com/example/demo/controller/RegisterController.java
+++ src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,6 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.dto.HeroForm;
+import com.example.demo.service.HeroListService;
 
 /**
  * @author yukari
@@ -11,4 +21,34 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class RegisterController {
 
+	@Autowired
+	private HeroListService service;
+	
+	/**
+	 * ヒーロー新規登録画面を表示
+	 * @param model Model
+	 * @return ヒーロー情報一覧画面
+	 */
+	@RequestMapping(value = "/hero/add", method = RequestMethod.GET)
+	public String heroAdd(Model model) {
+		model.addAttribute("heroForm", new HeroForm());
+		return "hero/add";
+	}
+	
+	/**
+	 * ヒーロー新規登録
+	 * @param heroForm フォームデータ
+	 * @param model Model
+	 * @return ヒーロー情報一覧画面
+	 */
+	@RequestMapping(value="/hero/add", method=RequestMethod.POST)
+    public String heroCreate(@Validated @ModelAttribute HeroForm heroForm, BindingResult result, Model model) {
+		//入力値のエラー判断
+		if (result.hasErrors()) {
+			return "/hero/add";
+		}
+		// ヒーロー情報の登録
+		service.register(heroForm);
+		return "redirect:/index";
+	}
 }

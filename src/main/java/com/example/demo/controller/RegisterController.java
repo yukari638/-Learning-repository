2aa4e8547db8286f52321e368e1c2dafc793f45f
yukari638 <khmk0118@gmail.com>
2021-02-22 12:00:42 +0900

    追加：
    コントローラーに、RegisterControllerクラスを新規作成
    ヒーロー新規登録画面用のコントローラークラスです。

---
+++ src/main/java/com/example/demo/controller/RegisterController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author yukari
+ *
+ * ヒーロー新規登録画面のコントローラー
+ */
+
+@Controller
+public class RegisterController {
+
+}

4cf82598794e696f3b79341c9abed873ad13496f
yukari638 <khmk0118@gmail.com>
2021-02-22 11:41:27 +0900

    追加：
    HeroMapperの内容をServiceクラスから呼び出せるようにコードを追加。

--- src/main/java/com/example/demo/service/HeroListService.java
+++ src/main/java/com/example/demo/service/HeroListService.java
@@ -1,6 +1,14 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dto.Hero;
+import com.example.demo.dto.HeroForm;
+import com.example.demo.mapper.HeroMapper;
 
 /*
  * ヒーローリストに関わるサービスクラスです。
@@ -11,7 +19,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class HeroListService {
 	
+	/* ヒーロー登録情報一覧のリポジトリ */
+	@Autowired
+	private HeroMapper mapper;
 	
+	/*
+	 * ヒーロー登録情報一覧を全件検索して取得します。
+	 * 
+	 * @return ヒーロー登録情報の全件取得
+	 */
+	public List<Hero> searchAll() {
+		return mapper.selectAll();
+	}
 	
+	/*
+	 * ヒーロー情報を登録します。
+	 * 
+	 * @param heroForm 登録するヒーロー情報
+	 */
+	@Transactional
+	public void register(HeroForm heroForm) {
+		mapper.insert(heroForm);
+	}
 	
 }
